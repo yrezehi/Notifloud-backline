cb0db98c9bf457b8fdf36e0152a35e099dc365d3
unknown <yrezehi@gmail.com>
2023-10-12 08:22:54 -0700

    setup: base class

--- Notifloud-dotnet/NotifloudToast.cs
+++ Notifloud-dotnet/NotifloudToast.cs
@@ -8,5 +8,18 @@ namespace Notifloud_dotnet
 {
     public class NotifloudToast
     {
+        public string Title { get; set; }
+        public string Message { get; set; }
+
+        public NotifloudToast(string title, string message)
+        {
+            Title = title;
+        }
+        
+        public NotifloudToast(string message)
+        {
+            Message = message;
+        }
+
     }
 }
