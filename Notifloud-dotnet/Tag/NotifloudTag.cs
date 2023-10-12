736cff2dc0eae9213fb273c9a6d5d222b9987be5
unknown <yrezehi@gmail.com>
2023-10-12 08:17:39 -0700

    setup: base

---
+++ Notifloud-dotnet/Tag/NotifloudTag.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Notifloud_dotnet.Tag
+{
+    public class NotifloudTag : TagHelper
+    {
+        private static readonly string CLASS_NAME = "toast";
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "div";
+
+            output.Attributes.Add("class", CLASS_NAME);
+
+            base.Process(context, output);
+        }
+    }
+}
