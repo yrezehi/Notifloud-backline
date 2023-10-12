dcd083e847655fdff5c7e8e90cc2e36a566a6fd8
unknown <yrezehi@gmail.com>
2023-10-12 08:29:39 -0700

    setup: base class notificaiton

---
+++ Notifloud-dotnet/Notifloud.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notifloud_dotnet
+{
+    public class Notifloud
+    {
+        public IList<NotifloudToast> Toasts { get; set; }
+
+        public Notifloud() =>
+            Toasts = new List<NotifloudToast>();
+
+        public void Success(string message)
+        {
+            Toasts.Add();
+        }
+
+        public void Error()
+        {
+
+        }
+
+        public void Information()
+        {
+
+        }
+
+        public void Warning() { 
+            
+        }
+    }
+}
