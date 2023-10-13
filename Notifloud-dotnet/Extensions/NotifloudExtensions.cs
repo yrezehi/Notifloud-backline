97abf7f58ba2c053850853452e1f6c1539cf3c2c
unknown <yrezehi@gmail.com>
2023-10-12 17:44:57 -0700

    setup: Extension

---
+++ Notifloud-dotnet/Extensions/NotifloudExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Notifloud_dotnet.Extenstions
+{
+    public static class NotifloudExtensions
+    {
+        public static void UserNotifloud(WebApplicationBuilder builder)
+        {
+            builder.Services.AddScoped(typeof(Notifloud), typeof(Notifloud));
+        }
+    }
+}
